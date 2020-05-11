3799c1e98c50618fe6334c77c9a858466752dfd0
busra <busrakgllr@hotmail.com>
2020-05-12 00:46:53 +0300

    deneme1

---
+++ lib/main.dart
@@ -0,0 +1,12 @@
+import 'package:flutter/material.dart';
+import 'package:phone_book4/contact_page.dart';
+
+import 'contact_page.dart';
+
+void main() => runApp(MyApp());
+
+class MyApp extends StatelessWidget {
+  Widget build(BuildContext context) {
+    return MaterialApp(home: ContactPage());
+  }
+}
